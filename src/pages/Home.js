543b250c21802013a19ec59db8fce76337d47724
Oluyedi Oluwasegun Stephen <oluyedis@gmail.com>
2022-10-04 13:51:38 +0100

    quick fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,11 @@ export default function Home() {
     const navigate = useNavigate()
   return (
 <section className="hero-section">
-    <div className="items-center justify-items-center grid w-full px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+    <div className="items-center justify-items-center grid w-full px-4 py-8 mx-auto lg:gap-8 lg:grid-cols-12">
     <div className="lg:mt-0 lg:col-span-5 lg:order-first md:order-last sm:order-last">
             <img src={heroImage} alt=""/>
         </div>
-        <div className="mr-auto lg:col-span-7">
+        <div className=" lg:col-span-7">
             <h1 style={{'color':'#240D57'}}className="max-w-2xl mb-4 text-[64px] font-extrabold tracking-tight leading-none  xl:text-[64px] dark:text-white">Imagine if <span style={{  'background': 'linear-gradient(45.34deg, #EA52F8 5.66%, #0066FF 94.35%)', 'backgroundSize': '100%',
     'backgroundClip': 'text',
     'textFillColor': 'transparent',
