849d9d2b4f7fd38ec4d67b11c917d4dd6ccd3508
Oluyedi Oluwasegun Stephen <oluyedis@gmail.com>
2022-10-04 09:41:58 +0100

    quick fix

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import './App.css';
 import Home from './pages/Home';
 import Create from './pages/Create'
 import Events from './pages/Events'
-// import {Router, Routes, Route} from 'react-router-dom'
+import { Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    // <Router>
     <div className="App">
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={ <Home/> } />
          <Route path="create" element={ <Create/> } />
         <Route path="events" element={ <Events/> } />
-    </Routes> */}
-      <Home />
+    </Routes>
+      {/* <Home />
       <Create />
-      <Events /> 
+      <Events />  */}
     </div>
-    // </Router>
 
   );
 }
