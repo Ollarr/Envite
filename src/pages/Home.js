2fc6425872c298d5fd24f6ddb290b10d2319890e
Oluyedi Oluwasegun Stephen <oluyedis@gmail.com>
2022-10-04 09:46:13 +0100

    quick fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import heroImage from '../assets/image 1.png';
+import { Link } from 'react-router-dom';
 export default function Home() {
   return (
 <section className="hero-section">
@@ -10,9 +11,8 @@ export default function Home() {
         <div className="mr-auto place-self-center lg:col-span-7">
             <h1 style={{'color':'#240D57'}}className="max-w-2xl mb-4 text-[64px] font-extrabold tracking-tight leading-none  xl:text-[64px] dark:text-white">Imagine if <span style={{  'background': 'linear-gradient(45.34deg, #EA52F8 5.66%, #0066FF 94.35%)'}}>snatchat</span> had events.</h1>
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Easily host and share events with your friends across any social media..</p>
-            <button style={{'backgroundImage': 'linear-gradient(#8456EC, #E87BF8)','padding': '16px', 'borderRadius':'10px', 'color':'#FFFFFF'}}>create my event</button>
+           <Link to= '/create'> <button style={{'backgroundImage': 'linear-gradient(#8456EC, #E87BF8)','padding': '16px', 'borderRadius':'10px', 'color':'#FFFFFF'}}>create my event</button></Link>
         </div>
-                        
     </div>
 </section>  )
 }
